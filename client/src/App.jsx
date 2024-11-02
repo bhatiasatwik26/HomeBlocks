f3063d11f70dfaebfa446937b4a3467bf2ac6fb7
Satwik Bhatia <satwikbhatia26@gmail.com>
2024-11-02 23:59:05 +0530

    created header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+import Header from "./pages/Header";
 
-const App = () => {
+export default function App() {
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<SignUp/>} />
+      </Routes>  
+    </BrowserRouter>
   )
 }
-
-export default App
---
+++ client/src/pages/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { HiMiniMagnifyingGlass } from "react-icons/hi2";
+
+const Header = () => {
+  return (
+    <header className='bg-slate-300'>
+        <div className="container flex justify-between items-center p-4 ">
+            <h1 className='text-2xl font-semibold'>
+                <span className='text-slate-700'>Home</span>
+                <span className='text-slate-600'>|</span>
+                <span className='text-slate-500'>Blocks</span>
+            </h1>
+            <form className='bg-slate-200 flex items-center justify-between px-3 py-2 rounded-lg w-1/2'>
+                <input type="text" placeholder='Search...' className='bg-transparent outline-none flex-1'/>
+                <HiMiniMagnifyingGlass className='text-lg'/>
+            </form>
+            <h1 className='font-semibold text-lg text-slate-400 cursor-pointer hover:text-slate-600 duration-150 '>Sign-In</h1>
+        </div>
+    </header>
+  )
+}
+
+export default Header
