d3381762ba41a5521cd967c5eb4b86667969509d
Satwik Bhatia <satwikbhatia26@gmail.com>
2024-11-04 22:08:28 +0530

    connected DB

---
+++ api/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+    .then(()=>{console.log('Connected to DB')})
+    .catch(()=>{console.log('Caught Error')})
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log('Server on port 3000');
+});
